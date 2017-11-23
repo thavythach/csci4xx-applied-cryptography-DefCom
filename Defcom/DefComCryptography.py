1e85d9d748731254a934f918c40e5ae04ddce4e4
Thavy Thach <thavythach@gmail.com>
2017-11-23 07:08:32 +0100

    DefComCryptography TestCryptography UnitTesting added.

--- Defcom/DefComCryptography.py
+++ Defcom/DefComCryptography.py
@@ -16,8 +16,10 @@ def Encrypt( _buffer, keystring ):
 	Encryption of buffer
 	'''
 
-	# TODO: EDGE CASE: buffer... format??? I think it's just a password
-	# TODO: EDGE CASE: keystring... at least 32 bits restriction
+	if len(keystring) == 32:
+		pass
+	else:
+		raise KeyError('keystring argument must be 32 bytes.')
 
 	# TLS style padding
 	plength = AES.block_size - ( len (_buffer) )% AES.block_size;
@@ -49,7 +51,11 @@ def Decrypt( e_buffer, keystring ):
 	'''
 	
 	# TODO: EDGE CASE: buffer... format??? I think it's just a password
-	# TODO: EDGE CASE: keystring... at least 32 bits restriction
+	# TODO: EDGE CASE: keystring... at least 32 bits restriction`
+	if len(keystring) == 32:
+		pass
+	else:
+		raise KeyError('keystring argument must be 32 bytes.')
 
 	# read in encrypted payload
 	e_buffer = b64decode(e_buffer)
@@ -65,25 +71,48 @@ def Decrypt( e_buffer, keystring ):
 	_buffer = cipher.decrypt( e_buffer )
 	
 	# remove TLS padding
-	__buffer = _buffer[:len(_buffer)-ord(_buffer[-1])]
+	__buffer = _buffer[:len(_buffer)-_buffer[-1]]
 	
 	# return __buffer
-	return __buffer
+	return __buffer.decode('utf-8')
 
 class TestCryptography(unittest.TestCase):
+	'''
+	TestCases for Cryptography Encrypt() and Decrypt() Functions
+
+	Key Arguments:
+	unittest.TestCase - inheritance in use
+	'''
 	
 	def setUp( self ):
-		self.keystring = '0123456789abcdefghijklmnopqrstwv'
 		self.password = 'skylarlevey'
 
+		self.keystrings = [
+			'0123456789abcdefghijklmnopqrstwv', # 32 perfect
+			'0123456789abcdefghijklmnopqrstw', # 31 perfect-1
+			'0123456789abcdefghijklmnopqrstwvx' # 34 perfect+1	
+		]
+
 	def testEncryption( self ):
-		ENC = Encrypt( _buffer=self.password, keystring=self.keystring )		
+		ENC = Encrypt( _buffer=self.password, keystring=self.keystrings[0] )		
 		self.assertNotEqual( self.password, ENC )
 	
 	def testDecryption( self ):
-		ENC = Encrypt( _buffer=self.password, keystring=self.keystring )		
-		DEC = Decrypt( e_buffer=ENC, keystring=self.keystring )
+		ENC = Encrypt( _buffer=self.password, keystring=self.keystrings[0] )
+		DEC = Decrypt( e_buffer=ENC, keystring=self.keystrings[0] )
 		self.assertEqual( self.password, DEC )
+	
+	def testEncryption_non_32_bit_key( self ):
+		self.assertRaises( KeyError, Encrypt, self.password, self.keystrings[1] )
+		self.assertRaises( KeyError, Encrypt, self.password, self.keystrings[2] )
+	
+	def testDecryption_non_32_bit_key( self ):
+		
+		## 32 bit key
+		ENC = Encrypt( _buffer=self.password, keystring=self.keystrings[0] )
+		## 31 bit key
+		self.assertRaises( KeyError, Decrypt, e_buffer=ENC, keystring=self.keystrings[1] )
+			
 
 if __name__ == "__main__":
 	unittest.main()
