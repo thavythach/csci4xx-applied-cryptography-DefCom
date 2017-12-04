52fbf15a0b7f1404503c32554be1661a4565f96f
Thavy Thach <thavythach@gmail.com>
2017-12-04 22:43:36 +0100

    new user generation

--- Defcom/mastermind.py
+++ Defcom/mastermind.py
@@ -3,25 +3,51 @@ import json
 import os
 
 def create_new_user( username, plaintext_password ):
-    	'''
-
+	'''
 	RETURNS A JSON OBJECT of credentials the user ONLY HAS
 	'''
 	client_priv, client_pub = generate_RSA()
 	
 	cert = Generate32BitKey()
 	# print( cert )
-	return json.dumps({
+	return {
 		"user_name": username,
 		"password": plaintext_password,
 		"public_key": client_pub,
 		"private_key": client_priv,
-		"certificate":  cert.decode('unicode-escape') # TODO fix cert.....
-	})
+		"certificate":  cert.decode('unicode-escape') 
+		# encrypted_hash of the public_key and encrypted with the CA's private key
+	}
 
 if __name__ == "__main__":
-    # for idx in range(0,10):
-    cwd = os.getcwd()
-    print( cwd )
-    if not os.path.exists(cwd+'/gen_users'):
-        os.makedirs()
+	
+	user_pass = [
+		('bill', 'gates'),
+		('steve', 'jobs'),
+		('skylar', 'levey'),
+		('thavy', 'thach'),
+		('classical', 'cryptography'),
+		('quantum', 'cryptography')
+	]
+
+	for user, pw in user_pass:
+		data = create_new_user( username=user , plaintext_password=pw )
+
+		with open('user_' + user + ".json", 'w') as outfile:
+			json.dump(data, outfile)
+	
+	# TODO put in a user folder
+	# for idx in range(0,10):
+	# cwd = os.getcwd()
+	# print( cwd )
+	# client = cwd+'/client'
+	# server = cwd+'/server'
+
+	# if not os.path.exists(client):
+	#     os.makedirs(client)
+	
+	# os.fchdir(client)
+
+	# for user in range(0,5):
+	# 	creds = create_new_user( "Shirley_"+user, "bad_password_"+user )
+		
